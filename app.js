ce84cc5634ad8bf7723207007e76158f8eaea3c7
Xavi Belloso <xavi.belloso@gmail.com>
2013-05-31 07:46:01 +0200

    Update users screens

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
+  , user = require('./routes/users')
   , http = require('http')
   , path = require('path');
 
@@ -30,8 +30,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+require('./routes/index')(app);
+require('./routes/users')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
